ad4944e5cb0785d8e31e196df3dfc4de82700d73
Arkady Vinkovsky <66293881+vinkovsky@users.noreply.github.com>
2022-02-08 00:07:21 +0700

    error

--- tmp.js
+++ tmp.js
@@ -99,15 +99,15 @@ var __defProp = Object.defineProperty,
   //fullscreenQuad.vert
   var fullscreenQuad = {
     source: `
-  layout(location = 0) in vec2 a_position;
-
-  out vec2 vCoord;
-
-  void main() {
-    vCoord = a_position;
-    gl_Position = vec4(2. * a_position - 1., 0, 1);
-  }
-`,
+   layout(location = 0) in vec2 a_position;
+ 
+   out vec2 vCoord;
+ 
+   void main() {
+     vCoord = a_position;
+     gl_Position = vec4(2. * a_position - 1., 0, 1);
+   }
+ `,
   };
 
   //glUtils
